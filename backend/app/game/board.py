b15957087227b3bab75e24991e4ccb9161e2599d
Yash <amreliayash876@gmail.com>
2026-08-01 11:31:44 +0530

    Add Monopoly board data

---
+++ backend/app/game/board.py
@@ -0,0 +1,70 @@
+BOARD = [
+    {
+        "position": 0,
+        "name": "GO",
+        "type": "go"
+    },
+    {
+        "position": 1,
+        "name": "Mediterranean Avenue",
+        "type": "property",
+        "price": 60,
+        "rent": 2,
+        "owner": None
+    },
+    {
+        "position": 2,
+        "name": "Community Chest",
+        "type": "community_chest"
+    },
+    {
+        "position": 3,
+        "name": "Baltic Avenue",
+        "type": "property",
+        "price": 60,
+        "rent": 4,
+        "owner": None
+    },
+    {
+        "position": 4,
+        "name": "Income Tax",
+        "type": "tax",
+        "amount": 200
+    },
+    {
+        "position": 5,
+        "name": "Reading Railroad",
+        "type": "railroad",
+        "price": 200,
+        "owner": None
+    },
+    {
+        "position": 6,
+        "name": "Oriental Avenue",
+        "type": "property",
+        "price": 100,
+        "rent": 6,
+        "owner": None
+    },
+    {
+        "position": 7,
+        "name": "Chance",
+        "type": "chance"
+    },
+    {
+        "position": 8,
+        "name": "Vermont Avenue",
+        "type": "property",
+        "price": 100,
+        "rent": 6,
+        "owner": None
+    },
+    {
+        "position": 9,
+        "name": "Connecticut Avenue",
+        "type": "property",
+        "price": 120,
+        "rent": 8,
+        "owner": None
+    }
+]
