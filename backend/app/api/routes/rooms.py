f64837bb57f56dca282abdbcd8e512a8d8f862b9
Yash <amreliayash876@gmail.com>
2026-08-01 11:10:14 +0530

    Add get room by ID API

--- backend/app/api/routes/rooms.py
+++ backend/app/api/routes/rooms.py
@@ -1,6 +1,5 @@
+from fastapi import APIRouter, HTTPException
 from uuid import uuid4
-
-from fastapi import APIRouter
 
 from app.schemas.room import RoomCreate
 
@@ -24,9 +23,23 @@ def create_room(room: RoomCreate):
 
     return room_data
 
+
 @router.get("/rooms")
 def get_rooms():
     return {
         "total_rooms": len(rooms),
         "rooms": rooms
     }
+
+
+@router.get("/rooms/{room_id}")
+def get_room(room_id: str):
+
+    for room in rooms:
+        if room["room_id"] == room_id:
+            return room
+
+    raise HTTPException(
+        status_code=404,
+        detail="Room not found"
+    )
